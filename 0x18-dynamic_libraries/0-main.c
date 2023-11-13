6ab61ac2139a593ff2b360d50bef27c4e031c983
AhmedbenHussein <ahmed.h.elrawi@gmail.com>
2023-11-13 01:51:56 -0800

    libdynamic

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,7 +1,5 @@
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 #include <stdio.h>
-#include "lists.h"
 
 /**
  * main - check the code
@@ -10,24 +8,6 @@
  */
 int main(void)
 {
-dlistint_t *head;
-dlistint_t *new;
-dlistint_t hello = {8, NULL, NULL};
-size_t n;
-head = &hello;
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-{
-dprintf(2, "Error: Can't malloc\n");
-return (EXIT_FAILURE);
-}
-new->n = 9;
-head->prev = new;
-new->next = head;
-new->prev = NULL;
-head = new;
-n = print_dlistint(head);
-printf("-> %lu elements\n", n);
-free(new);
+printf("%d\n", _strlen("My Dyn Lib"));
 return (EXIT_SUCCESS);
 }
